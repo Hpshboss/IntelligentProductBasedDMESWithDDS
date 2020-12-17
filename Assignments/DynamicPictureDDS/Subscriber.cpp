0f1f37675f0d3a5d018bdd938eaa0ff2c233a858
Lu,Hong-Ji <49175517+Hpshboss@users.noreply.github.com>
2020-12-17 11:30:20 +0800

    Add files via upload

---
+++ Assignments/DynamicPictureDDS/Subscriber.cpp
@@ -0,0 +1,186 @@
+// Copyright 2016 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+
+// Hpshboss modifys code from eprosima's github example;
+// Licensed under the Apache License, Version 2.0 (the "License");
+
+/**
+ * @file Subscriber.cpp
+ *
+ */
+
+#include "Subscriber.h"
+#include <fastrtps/attributes/ParticipantAttributes.h>
+#include <fastrtps/attributes/SubscriberAttributes.h>
+#include <fastrtps/subscriber/Subscriber.h>
+#include <fastrtps/Domain.h>
+#include <fastrtps/types/DynamicTypeBuilderFactory.h>
+#include <fastrtps/types/DynamicDataFactory.h>
+#include <fastrtps/types/DynamicTypeBuilder.h>
+#include <fastrtps/types/DynamicTypeBuilderPtr.h>
+#include <fastrtps/types/DynamicType.h>
+
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iterator>
+
+#include <opencv2/opencv.hpp>
+
+using namespace eprosima::fastrtps;
+using namespace eprosima::fastrtps::rtps;
+using namespace eprosima::fastrtps::types;
+
+// using namespace cv;
+
+PictureSubscriber::PictureSubscriber()
+    : mp_participant(nullptr)
+    , mp_subscriber(nullptr)
+    , m_DynType(DynamicType_ptr(nullptr))
+{
+}
+
+struct timespec begin, end;
+unsigned int countSample = 0;
+double elapsed;
+std::vector<unsigned char> buffer;
+
+bool PictureSubscriber::init()
+{
+
+    ParticipantAttributes PParam;
+    PParam.rtps.setName("DynPicture_sub");
+    mp_participant = Domain::createParticipant(PParam, (ParticipantListener*)&m_part_list);
+    if (mp_participant == nullptr)
+    {
+        return false;
+    }
+
+    // Create basic builders
+    DynamicTypeBuilder_ptr struct_type_builder(DynamicTypeBuilderFactory::get_instance()->create_struct_builder());
+
+    DynamicTypeBuilder_ptr octet_builder(DynamicTypeBuilderFactory::get_instance()->create_byte_builder());
+    DynamicTypeBuilder_ptr sequence_type_builder(DynamicTypeBuilderFactory::get_instance()->create_sequence_builder(octet_builder.get(), 3873715));
+    DynamicType_ptr sequence_type = sequence_type_builder->build();
+
+    // Add members to the struct.
+    struct_type_builder->add_member(0, "index", DynamicTypeBuilderFactory::get_instance()->create_uint32_type());
+    struct_type_builder->add_member(1, "Picture", sequence_type);
+    struct_type_builder->set_name("Picture");
+    
+    DynamicType_ptr dynType = struct_type_builder->build();
+    m_DynType.SetDynamicType(dynType);
+    m_listener.m_DynHello = DynamicDataFactory::get_instance()->create_data(dynType);
+
+    //REGISTER THE TYPE
+    Domain::registerDynamicType(mp_participant, &m_DynType);
+
+    //CREATE THE SUBSCRIBER
+    SubscriberAttributes Rparam;
+    Rparam.topic.topicKind = NO_KEY;
+    Rparam.topic.topicDataType = "Picture";
+    Rparam.topic.topicName = "PictureTopic";
+
+    mp_subscriber = Domain::createSubscriber(mp_participant, Rparam, (SubscriberListener*)&m_listener);
+
+    if (mp_subscriber == nullptr)
+    {
+        return false;
+    }
+
+
+    return true;
+}
+
+PictureSubscriber::~PictureSubscriber()
+{
+    Domain::removeParticipant(mp_participant);
+
+    DynamicDataFactory::get_instance()->delete_data(m_listener.m_DynHello);
+
+    Domain::stopAll();
+}
+
+void PictureSubscriber::SubListener::onSubscriptionMatched(
+        Subscriber* /*sub*/,
+        MatchingInfo& info)
+{
+    if (info.status == MATCHED_MATCHING)
+    {
+        n_matched++;
+        std::cout << "Subscriber matched" << std::endl;
+    }
+    else
+    {
+        n_matched--;
+        std::cout << "Subscriber unmatched" << std::endl;
+    }
+}
+
+void PictureSubscriber::PartListener::onParticipantDiscovery(
+        Participant*,
+        ParticipantDiscoveryInfo&& info)
+{
+    if (info.status == ParticipantDiscoveryInfo::DISCOVERED_PARTICIPANT)
+    {
+        std::cout << "Participant " << info.info.m_participantName << " discovered" << std::endl;
+    }
+    else if (info.status == ParticipantDiscoveryInfo::REMOVED_PARTICIPANT)
+    {
+        std::cout << "Participant " << info.info.m_participantName << " removed" << std::endl;
+    }
+    else if (info.status == ParticipantDiscoveryInfo::DROPPED_PARTICIPANT)
+    {
+        std::cout << "Participant " << info.info.m_participantName << " dropped" << std::endl;
+    }
+}
+
+void PictureSubscriber::SubListener::onNewDataMessage(
+        Subscriber* sub)
+{
+    if (sub->takeNextData((void*)m_DynHello, &m_info))
+    {
+        if (m_info.sampleKind == ALIVE)
+        {
+            this->n_samples++;
+            countSample++;
+            
+            // Print your structure data here.
+            uint32_t index;
+            m_DynHello->get_uint32_value(index, 0);
+            std::cout << "index: " << index <<  std::endl;  
+            
+            
+            DynamicType_ptr octet_type_temp(DynamicTypeBuilderFactory::get_instance()->create_byte_type());
+            DynamicTypeBuilder_ptr sequence_type_builder_temp(DynamicTypeBuilderFactory::get_instance()->create_sequence_builder(octet_type_temp, 3873715));
+            DynamicType_ptr sequence_type_temp = sequence_type_builder_temp->build();
+
+            DynamicData* sequence_data_temp = m_DynHello->loan_value(1);
+            for (int i = 0; i < index; i++) {
+                buffer.push_back(sequence_data_temp->get_byte_value(i));
+            }
+            m_DynHello->return_loaned_value(sequence_data_temp);
+            
+            cv::Mat imageDecoded = cv::imdecode(buffer, 1);
+
+            cv::imwrite(std::to_string(countSample) + "_droneNew.jpg", imageDecoded);
+        }
+    }
+}
+
+void PictureSubscriber::run()
+{
+    std::cout << "Subscriber running. Please press enter to stop the Subscriber" << std::endl;
+    std::cin.ignore();
+}
+
+void PictureSubscriber::run(
+        uint32_t number)
+{
+    std::cout << "Subscriber running until " << number << "samples have been received" << std::endl;
+    while (number > this->m_listener.n_samples)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+}
